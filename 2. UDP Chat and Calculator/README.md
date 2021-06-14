61ca95c86dd565f8309107c12cc11e3848cd21ff
João Rafael <jrsc2@cin.ufpe.br>
2021-06-14 02:14:57 -0300

    Add UDP Chat

---
+++ 2. UDP Chat and Calculator/README.md
@@ -0,0 +1,24 @@
+# Exercício de Sockets  
+  
+1. Semelhante ao exercício anterior com Sockets TCP, agora você utilizará sockets UDP para criar uma aplicação console que funcionará como chat ponto a ponto, onde um cliente se conecta a um Servidor e troca mensagens.  
+Tanto no cliente como no servidor será possível digitar mensagens do chat a partir do console.  
+
+2. Considere um servidor que fornece um serviço de calculadora remota "avançada" que disponibiliza as operações artiméticas de adição, subtração, multiplicação e divisão.  
+Os aplicativos clientes que utilizam o serviço devem se conectar e enviar dois inteiros como parâmetro, junto com o parâmetro indicando qual operação deve ser efetuada.  
+O servidor devolve como resposta à aplicação cliente o resultado da operação. O cliente pode ser uma aplicação console que recebe os parâmetros e a operação através do terminal.  
+a. Implemente o cliente e servidor em TCP  
+b. Implemente o cliente e servidor em UDP  
+
+
+## Quais as principais diferenças entre TCP e UDP?  
+  
+## Quais as principais diferenças entre a implementação TCP e UDP (tanto do chat como da calculadora)?  
+  
+## Quais as principais dificuldades nas implementações UDP?  
+
+## Quando faz sentido usar TCP ou UDP?  
+
+
+
+Pontos interessantes sobre TCPxUDP na implementação de jogos:
+https://www.youtube.com/watch?v=OuSf5g3Kv68
---
+++ 2. UDP Chat and Calculator/chat/server.js
@@ -0,0 +1,111 @@
+const dgram = require("dgram")
+const readline = require('readline')
+
+const socket = dgram.createSocket("udp4")
+const users = {}
+
+class User {
+  constructor(info, name = null) {
+    this.info = info
+    this.name = name
+  }
+
+  sendMessage(message, sender) {
+    if (this.isNotRegistered()) return
+    if (this.isSelfMessage(sender)) return
+    this.send(message)
+  }
+
+  isSelfMessage(sender) {
+    return sender != null && sender.name == this.name
+  }
+
+  isNotRegistered() {
+    return this.name == null
+  }
+
+  send(message) {
+    socket.send(message, this.info.port, this.info.address)
+  }
+}
+
+socket.on("message", function(data, info) {
+  const message = data.toString()
+  if (!(info in users)) {
+    return addUser(info)
+  }
+  const user = users[info]
+  if (user.isNotRegistered()) {
+    return registerUser(user, message)
+  }
+  // Sends the message to everyone
+  broadcast(message, user)
+})
+
+
+function addUser(info) {
+  const user = new User(info)
+  users[info] = user
+  console.log("---New connection---")
+  user.send("Welcome! Please enter your name.")
+}
+
+function registerUser(user, message) {
+  // Shouldn't have duplicate usernames
+  if (message in getUsernames()) {
+    return user.send("This name is already being used!")
+  }
+  user.name = message
+  broadcast(user.name + " has joined the chat!")
+}
+
+function broadcast(message, sender) {
+  // "Name: Message" or "Message"
+  if (sender != null && sender.name != "") {
+    message = `${sender.name}: ${message}`
+  }
+  for (const user of getUsers()) {
+    user.sendMessage(message, sender)
+  }
+  // Send the message to the server
+  console.log(message)
+}
+
+socket.on("error", function(had_error) {
+  if (socket.name != null && had_error) {
+    console.log("---Error at " + socket.name + "---")
+  }
+})
+
+
+
+console.log("Server is open!")
+socket.bind(8081, "localhost")
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+rl.addListener('line', line => {
+  // Send the message to everyone as the server
+  for (const user of getUsers()) {
+    user.send("Server >>> " + line)
+  }
+})
+
+
+
+// Utils
+function getUsers() {
+  return Object.values(users)
+}
+
+function getUsernames() {
+  function getName(user) { return user.name }
+  const names = getUsers().map(getName)
+  function onlyRegistered(name) { return name != null }
+  return names.filter(onlyRegistered)
+}
+
+
